2af2f16efd5e5486e40316d675d26e9fd7480843
Ryan Kurte <ryankurte@gmail.com>
2015-05-05 10:03:05 +1200

    Added pwm stub

---
+++ firmware/source/pwm.c
@@ -0,0 +1,28 @@
+
+#include "pwm.h"
+
+#include <stdint.h>
+
+#include "em_cmu.h"
+#include "em_adc.h"
+
+void PWM_init() 
+{
+
+}
+
+void PWM_close()
+{
+
+
+}
+
+void PWM_enable(uint8_t channel, bool enable)
+{
+
+}
+
+void PWM_write(uint8_t channel) 
+{
+
+}
